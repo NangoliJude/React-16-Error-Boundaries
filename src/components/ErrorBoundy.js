dbad9257afb48e22c982dd02d9e17f3635daf5e3
Nangoli Jude Eric <fxjudemedia@gmail.com>
2018-04-29 17:08:43 -0700

    Update ErrorBoundy.js

--- src/components/ErrorBoundy.js
+++ src/components/ErrorBoundy.js
@@ -16,7 +16,6 @@ class ErrorBoundry extends Component{
     }
     render(){
         if(this.state.hasError){
-            console.log("error found")
             return <span>Opps!! Seen some Error</span>
             
 
